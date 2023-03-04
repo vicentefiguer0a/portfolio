b7ad0bcb649e1c9606a7f74e062b9da411eaa0a9
Vicente Figueroa <figueroa.vicente9@gmail.com>
2023-03-04 00:11:41 -0800

    refactored: updated link to employee management system project

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,5 +1,11 @@
 export default [
     {
+        title: 'Sorting Algorithm Visualizer',
+        imgUrl: 'assets/sorting-algorithm-visualizer.JPG',
+        stack: ['Java', 'Java Swing'],
+        link: 'https://github.com/vicentefiguer0a/sorting-algorithm-visualizer'
+    },
+    {
         title: 'The Locker Room',
         imgUrl: 'assets/lockerrm-project.JPG',
         stack: ['React', 'CSS', 'Bootstrap', 'Spring Boot', 'MySQL'],
@@ -9,12 +15,6 @@ export default [
         title: 'Employee Management System',
         imgUrl: 'assets/ems-project.JPG',
         stack: ['React', 'Tailwind', 'Spring Boot', 'MySQL'],
-        link: 'https://github.com/vicentefiguer0a/employee-management-system-ui'
-    },
-    {
-        title: 'Sorting Algorithm Visualizer',
-        imgUrl: 'assets/sorting-algorithm-visualizer.JPG',
-        stack: ['Java', 'Java Swing'],
-        link: 'https://github.com/vicentefiguer0a/sorting-algorithm-visualizer'
-    },
+        link: 'https://github.com/vicentefiguer0a/employee-management-system'
+    }
 ]
